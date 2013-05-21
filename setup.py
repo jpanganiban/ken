dc318bcbd34e3f1eb1cae92e705f443d084045f3
Jesse Panganiban <me@jpanganiban.com>
2013-05-21 22:05:07 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 
 
 setup(name="ken",
-      version="9001.0.1",
+      version="9001.0.2",
       description="Ken is over 9000",
       long_description=long_description,
       py_modules=["ken"],
