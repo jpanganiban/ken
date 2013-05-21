2cc1774f79fe2ad00af8fb498ad61526d3f27deb
Jesse Panganiban <me@jpanganiban.com>
2013-05-21 21:51:13 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name="ken",
+      version="9001.0.0",
+      long_description="Some really cool shiznit!",
+      description="Ken is over 9000",
+      py_modules=["ken"],
+      author="Infoshift Inc",
+      author_email="me@jpanganiban.com")
