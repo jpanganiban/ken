98b406aa189c0b22f033dd65b9d9fbaaceb86fa2
Jesse Panganiban <me@jpanganiban.com>
2013-05-21 21:54:44 +0800

    proper long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
 setup(name="ken",
       version="9001.0.0",
-      long_description="Some really cool shiznit!",
       description="Ken is over 9000",
+      long_description=long_description,
       py_modules=["ken"],
       author="Infoshift Inc",
       author_email="me@jpanganiban.com")
